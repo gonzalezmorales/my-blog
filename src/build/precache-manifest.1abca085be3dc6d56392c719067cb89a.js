49a26a0579e086028cc6596d3f4480521c0eb4f7
Luis Gonzalez Morales <gonzalezmorales@un.org>
2020-01-05 19:31:48 -0500

    first commit

---
+++ src/build/precache-manifest.1abca085be3dc6d56392c719067cb89a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3d6e6d60f04b75e17ec7f66df5c750b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "085c0ae05799bec07c42",
+    "url": "/static/css/main.7b5f7dc8.chunk.css"
+  },
+  {
+    "revision": "e3456c03bbaf969b866b",
+    "url": "/static/js/2.4f3e10bd.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.4f3e10bd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "085c0ae05799bec07c42",
+    "url": "/static/js/main.6e6c6580.chunk.js"
+  },
+  {
+    "revision": "2829fc8889d8b91e8c4b",
+    "url": "/static/js/runtime-main.b519a9b9.js"
+  }
+]);
